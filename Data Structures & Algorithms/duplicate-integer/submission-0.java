61faa30de8be95a4ddf210866be1d39d55360306
Anshif <ansh988820@gmail.com>
2026-07-15 20:20:34 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet<Integer> ans=new HashSet<>();
+
+        for(int num:nums){
+            if(!ans.add(num)){
+                return true;
+
+            }
+        }
+
+        return false;
+
+        
+    }
+}
